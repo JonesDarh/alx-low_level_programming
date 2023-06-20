bdb5882ebb433337cfc552389658d65aa460f551
“Darhour <darhour.younes@gmail.com>
2023-06-20 07:27:26 -0700

    6. There is no such thing as absolute value in this world

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,15 +5,12 @@
  * @n: the int to check
  * Return: the absolute value of int
  */
-
-int _abs(int c)
+int _abs(int n)
 {
-	if (c < 0)
-	{
-	int abs_val;
-        
-        abs_val = c * -1;	
-	return (abs_val);
-	}
-	return (c);
+    if (n < 0)
+    {
+        int abs_val = n * -1;
+        return abs_val;
+    }
+    return n;
 }
