a1b3fb86e44a74871c61c3bcd1a6f0edae95a037
“Darhour <darhour.younes@gmail.com>
2023-06-20 07:20:23 -0700

    7. There are only 3 colors

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,18 +5,19 @@
  * @n: the int to extract the last digit from
  * Return: value of the last digit
  */
-
 int print_last_digit(int n)
-
 {
-	
-	int last;
+	int a;
 
-	last = n % 10;
-	if (last < 0)
-	{
-	last = last * -1;
-        }
-        _putchar(last + '0');
-        return (last);
+	if (n < 0)
+	n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
