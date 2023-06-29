e2068d860a6996ed19485b740b361e8716406e33
“Darhour <darhour.younes@gmail.com>
2023-06-29 08:45:56 -0700

    10. A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -17,7 +17,7 @@ int main(void)
  * - only one statement
  * - you are not allowed to code anything else than this line of code
  */
-	memcpy(p + 6, &(int){98}, sizeof(int));
+	*(p + 5) = 98;
 /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
